42c39527e28709b2937d9f7dc080821be4c07e95
John Rhey <johnrheybayotlang324@gmail.com>
2026-02-19 03:08:51 +0800

    Added my components to the App.jsx file

--- JohnRhey-CV/src/App.jsx
+++ JohnRhey-CV/src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
+import About from './components/About'
+import Hobbies from './components/Hobbies'
+import Experience from './components/Experience'
+import Skills from './components/Skills'
 
 function App() {
 
   return (
     <>
-      <div className='header'>
-        <h1>John Rhey R. Bayotlang</h1>  
-      </div>
-
-      <div className='content'>
-        <h2>About Me</h2>
-        <p>Hi! I'm John Rhey, a passionate and dedicated software developer with a strong background in web development. I have experience working with various programming languages and frameworks, and I am always eager to learn new technologies. I thrive in collaborative environments and enjoy solving complex problems through code. My goal is to create efficient and user-friendly applications that make a positive impact on users' lives.</p> 
-      </div> 
-      
+      <Navbar />
+      <Profile />
+      <About />
+      <Hobbies />
+      <Experience />
+      <Skills />
     </>
   )
 }
