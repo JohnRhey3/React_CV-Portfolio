12d1c9e40ea2e0bd8345a0778ad97a72d5101871
John Rhey <johnrheybayotlang324@gmail.com>
2026-02-18 18:05:21 +0800

    initial commit

---
+++ JohnRhey-CV/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='header'>
+        <h1>John Rhey R. Bayotlang</h1>  
+      </div>
+
+      <div className='content'>
+        <h2>About Me</h2>
+        <p>Hi! I'm John Rhey, a passionate and dedicated software developer with a strong background in web development. I have experience working with various programming languages and frameworks, and I am always eager to learn new technologies. I thrive in collaborative environments and enjoy solving complex problems through code. My goal is to create efficient and user-friendly applications that make a positive impact on users' lives.</p> 
+      </div> 
+      
+    </>
+  )
+}
+
+export default App
