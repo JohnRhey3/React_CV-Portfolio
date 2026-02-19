4f99c6b58b59f6b2fbac1376e4f1ad329cf65982
John Rhey <johnrheybayotlang324@gmail.com>
2026-02-19 19:22:42 +0800

    linking icons to external URLs

--- JohnRhey-CV/src/components/Profile.jsx
+++ JohnRhey-CV/src/components/Profile.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Routes, Route, useLocation } from 'react-router-dom';
 import bg from "../assets/bg.png";
 import profImg from "../assets/1BAI1.png";
 import { FaFacebookSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
@@ -11,7 +12,7 @@ const Profile = () => {
     >
       <div className="profile-card">
 
-        {/* Left: photo only */}
+        
         <div className="profile-left">
           <img
             src={profImg}
@@ -20,7 +21,7 @@ const Profile = () => {
           />
         </div>
 
-        {/* Right: all text + icons */}
+        
         <div className="profile-right">
           <h2>John Rhey R. Bayotlang</h2>
           <p className="profile-title">Information Technology Intern</p>
@@ -28,9 +29,13 @@ const Profile = () => {
           <p>Phone: <span>09123456789</span></p>
 
           <div className="social-icons">
-            <a href="#" aria-label="Facebook"><FaFacebookSquare size={26} /></a>
-            <a href="#" aria-label="Instagram"><FaInstagram size={26} /></a>
-            <a href="#" aria-label="LinkedIn"><FaLinkedin size={26} /></a>
+            
+            <a href="https://www.facebook.com/john.rhey.7587" target="_blank" rel="noreferrer" aria-label="Facebook"><FaFacebookSquare size={26} /></a>
+
+            <a href="https://www.instagram.com/johnrheyyy/" target="_blank" rel="noreferrer" aria-label="Instagram"><FaInstagram size={26} /></a>
+
+            <a href="https://www.linkedin.com/in/john-rhey-bayotlang-253556364/" target="_blank" rel="noreferrer" aria-label="LinkedIn"><FaLinkedin size={26} /></a>
+
           </div>
         </div>
 
